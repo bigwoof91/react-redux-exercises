ae3e03d853ef50177d46880cb3aa9f334c435eb1
Michael <wolflearned91@gmail.com>
2017-08-01 17:26:09 -0400

    added helpers and heroes reducer

--- supersquad/src/actions/index.js
+++ supersquad/src/actions/index.js
@@ -3,7 +3,7 @@ export const ADD_CHARACTER = 'ADD_CHARACTER';
 export function addCharacterById(id) {
     const action = {
         type: ADD_CHARACTER,
-        id: id
+        id
     }
     return action;
 }
