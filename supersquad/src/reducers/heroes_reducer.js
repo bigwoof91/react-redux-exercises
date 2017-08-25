a15daa977abba18b802174bb4bb1183017198d0f
Michael <wolflearned91@gmail.com>
2017-08-25 18:15:23 -0400

    hero list functionality complete

--- supersquad/src/reducers/heroes_reducer.js
+++ supersquad/src/reducers/heroes_reducer.js
@@ -1,11 +1,16 @@
 import { ADD_CHARACTER } from '../actions';
+import { REMOVE_HERO } from '../actions'
 import { createCharacter } from './helpers.js';
+
 
 function heroes(state = [], action) {
     switch (action.type) {
         case ADD_CHARACTER:
             let heroes = [...state, createCharacter(action.id)];
             return heroes;
+        case REMOVE_HERO:
+            heroes = state.filter(item => item.id !== action.id);
+        return heroes;
         default:
             return state;
     }
